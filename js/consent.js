beb13f2baa16525214b13f4b40f7d75814895f8f
AlexAlifanov121 <a821ka@gmail.com>
2026-07-31 19:58:58 +0400

    sync mirror with live site 30.07.2026

--- js/consent.js
+++ js/consent.js
@@ -143,8 +143,6 @@
   }
 
   function init() {
-    /* RU-zerkalo: Yandex.Metrica starts immediately - required for Yandex Direct. */
-    loadYandex();
     var consent = readConsent();
     if (consent && consent.analytics) { fireAnalytics(); return; }
     if (consent && consent.analytics === false) { return; }
